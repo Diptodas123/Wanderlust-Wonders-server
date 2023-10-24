7774487a55b6d5cc67603ccfc13a35d1f02f2176
dipto-das <91727309+Diptodas123@users.noreply.github.com>
2023-10-25 05:13:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/', Routes);
-app.use('/uploads', express.static('upload'));
+app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
